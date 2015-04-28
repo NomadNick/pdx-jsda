0c059e7132d3f16f3992ef93a735f57635ee4022
NomadNick <emailnick123@gmail.com>
2015-04-28 13:33:02 -0700

    initial commit with package.json

---
+++ test/calc_test.js
@@ -0,0 +1,30 @@
+var calc = require('../calc.js');
+var expect = require('chai').expect;
+
+describe('calc', function() {
+  var a = 4, b = 3;
+  it('should add when called', function() {
+    expect(calc.add(a,b)).to.eql(7);
+  });
+})
+
+describe('calc', function() {
+  var a = 8, b = 3;
+  it('should subtract when called', function() {
+    expect(calc.subtract(a,b)).to.eql(5);
+  });
+})
+
+describe('calc', function() {
+  var a = 3, b = 3;
+  it('should multiply when called', function() {
+    expect(calc.multiply(a,b)).to.eql(9);
+  });
+})
+
+describe('calc', function() {
+  var a = 4, b = 2;
+  it('should divide when called', function() {
+    expect(calc.divide(a,b)).to.eql(2);
+  });
+})
